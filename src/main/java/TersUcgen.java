f49312703942078efaae0a8035b6fce55df8a9a3
Beyza Karaman <beyzak@posta.mu.edu.tr>
2025-03-07 15:27:04 +0300

    m

--- src/main/java/TersUcgen.java
+++ src/main/java/TersUcgen.java
@@ -1,11 +1,20 @@
+import java.util.Scanner;
+
 public class TersUcgen {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-        for(int i = 10; i > 0 ; i--){
-            for (int j = 0; j < i;j++){
+        System.out.print("Basamak sayısını girin: ");
+        int n = scanner.nextInt();
+
+        for (int i = n; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
+
+        scanner.close();
     }
 }
+
